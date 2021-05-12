0c11a4f450fa9b762a734376936a02cbe32a23bb
Pavel Surkov <SurPaul@yandex.ru>
2021-05-12 15:32:49 +0400

    Configuration with a single command

---
+++ README.md
@@ -0,0 +1,6 @@
+# Linux.conf
+Репозиторий для быстрой настройки Linux на новых компьютерах и серверах.
+
+```bash
+sh -c "wget $(https://raw.githubusercontent.com/SurPaul/Linux.conf/main/configure.sh)"
+```
--- configure.sh
+++ configure.sh
@@ -3,12 +3,14 @@
 
 sudo dpkg-reconfigure tzdata
 
-cp ./neofetch.conf ~/.config/neofetch/config.conf
-cp ./zshrc ~/.zshrc
-
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y neofetch ripgrep tmux vim wget zsh zplug
+sudo apt install -y neofetch ripgrep tmux vim wget zplug
+
+git clone https://github.com/SurPaul/Linux.conf.git
+cp ./Linux.conf/neofetch.conf ~/.config/neofetch/config.conf
+cp ./Linux.conf/zshrc ~/.zshrc
+rm -r Linux.conf
 
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 sudo chsh -s /bin/zsh $(whoami)
