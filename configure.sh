a99d7f320532546620470a85c6fba45674d8b9c2
Pavel Surkov <SurPaul@yandex.ru>
2021-05-12 15:44:05 +0400

    rm rf

--- configure.sh
+++ configure.sh
@@ -10,7 +10,7 @@ sudo apt install -y neofetch ripgrep tmux vim zplug
 git clone https://github.com/SurPaul/Linux.conf.git
 cp ./Linux.conf/neofetch.conf ~/.config/neofetch/config.conf
 cp ./Linux.conf/zshrc ~/.zshrc
-rm -r Linux.conf
+rm -rf Linux.conf
 
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 sudo chsh -s /bin/zsh $(whoami)
