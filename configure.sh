d43d104a136ff5d834969c2ac3cf497c2049f276
Pavel Surkov <SurPaul@yandex.ru>
2024-07-22 01:54:58 +0400

    omz instead of zplug, new packages

--- configure.sh
+++ configure.sh
@@ -14,10 +14,19 @@ sudo localectl set-locale LANG=en_US.UTF-8
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y fzf neofetch tmux vim zplug
+sudo apt install -y neofetch tmux nvim zsh bat git
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cd $HOME/.oh-my-zsh/custom/plugins/
+git clone https://github.com/catppuccin/zsh-syntax-highlighting.git catppuccin-zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-completions.git
 
 sudo chsh -s /bin/zsh $(whoami)
 
+cd
 git clone https://github.com/SurPaul/Linux.conf.git
 mkdir -p ~/.config/neofetch
 cp ./Linux.conf/neofetch.conf ~/.config/neofetch/config.conf
